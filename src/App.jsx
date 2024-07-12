20a7fc2ed9cb752d48d2a3c2246f1862e621914f
Rafi Zaman <rafiirfan211@gmail.com>
2024-07-12 16:51:30 +0600

    fetched cabin data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import Dashboard from "./pages/Dashboard.jsx";
 import Bookings from "./pages/Bookings.jsx";
 import Cabins from "./pages/Cabins.jsx";
@@ -10,9 +13,18 @@ import Account from "./pages/Account.jsx";
 import GlobalStyles from "./styles/GlobalStyles";
 import AppLayout from "./ui/AppLayout.jsx";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+});
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -30,7 +42,7 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,17 +1,18 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { useEffect } from "react";
-import { getCabins } from "../services/apiCabins.js";
+import CabinTable from "../features/cabins/CabinTable.jsx";
 
 function Cabins() {
-  useEffect(function () {
-    getCabins().then((data) => console.log(data)), [];
-  });
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
